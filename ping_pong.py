58b6478114b16f35030e63352f08630da3b67b19
MatveyPin <157215341+MatveyPin@users.noreply.github.com>
2024-02-02 20:39:17 +0200

    Update ping_pong.py
    Доданий таймер, умова нічиї, імпортовано модуль time.

--- ping_pong.py
+++ ping_pong.py
@@ -1,4 +1,7 @@
 import pygame
+import time
+
+pygame.font.init()
 
 ground = (0, 204, 255)
 window = pygame.display.set_mode((700, 500))
@@ -41,10 +44,12 @@ player2 = Player("platform.png", 320, 200, 7, 100, 50)
 font = pygame.font.SysFont("Arial", 26)
 text = font.render("Player2 won", True, (255, 0, 0))
 text2 = font.render("Player1 won", True, (255, 0, 0))
+none = font.render("Нічия", True, (255, 0, 0))
 game = True
 clock = pygame.time.Clock()
 fps = 60
 finish = False
+start_time = time.time()
 
 while game:
     
@@ -60,6 +65,11 @@ while game:
         player2.update1()
         ball.reset()
 
+        timee = 60 - (time.time() - start_time)
+        seconds = int(timee % 60)
+        time_text = font.render("Час:" + str(seconds), True, (255, 255, 255))
+        window.blit(time_text, (290, 10))
+
         if ball.rect.y < -20 or ball.rect.y >= 430:
             ball.speed_y = -ball.speed_y
         if ball.rect.x < -75:
@@ -71,6 +81,9 @@ while game:
         if pygame.sprite.collide_rect(ball, player1) or pygame.sprite.collide_rect(ball, player2):
             ball.speed_x = -ball.speed_x
             ball.speed_y = -ball.speed_y
+        if seconds == 0:
+            finish = True
+            window.blit(none, (270, 250))
     
     pygame.display.update() 
             
